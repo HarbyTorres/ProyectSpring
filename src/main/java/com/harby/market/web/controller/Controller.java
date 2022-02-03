68bfdc29099954a917f0f79f3ccafb97e71b8dca
HarbyTorres <habytorres29@gmail.com>
2022-02-03 09:39:54 -0500

    commit inicial

---
+++ src/main/java/com/harby/market/web/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.harby.market.web.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class Controller {
+
+    @GetMapping("/hola")
+    public String saludar(){
+        return ("hola mundo");
+    }
+}
