0f28c94d8ba2dbdeb5f26ca753d2abc1d21300f9
HarbyTorres <habytorres29@gmail.com>
2022-02-10 09:47:14 -0500

    Producto repository

--- src/main/java/com/harby/market/persistence/ProductoRepository.java
+++ src/main/java/com/harby/market/persistence/ProductoRepository.java
@@ -1,10 +1,14 @@
 package com.harby.market.persistence;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.harby.market.persistence.crud.ProductoCrudRepository;
 import com.harby.market.persistence.entity.Producto;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -15,6 +19,34 @@ public class ProductoRepository {
 
     public List<Producto> getByCategoria(int idCategoria) {
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getByStock (int cantidadStock){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, true);
+    }
+
+    public Optional<List<Producto>> getByNombre (String nombre){
+        return productoCrudRepository.findByNombreContainingOrderByNombreAsc(nombre);
+    }
+    
+    public List<Producto> getByEstado (boolean estado){
+        return productoCrudRepository.findByEstado(estado);
+    }
+
+    public List<Producto> getByBetweenPrecioVenta (int min, int max){
+        return productoCrudRepository.findByPrecioVentaBetween(min, max);
+    }
+
+    public Optional<Producto> getProducto (int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
     
 }
--- src/main/java/com/harby/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/harby/market/persistence/crud/ProductoCrudRepository.java
@@ -1,15 +1,25 @@
 package com.harby.market.persistence.crud;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.harby.market.persistence.entity.Producto;
 
 import org.springframework.data.repository.CrudRepository;
 
+//repositorio Spring Data
 /**CrudRepository, PagingAndSortingRepository, JPARepository**/
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer>{ 
 
     List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado( int cantidadStock, boolean estado);
+
+    Optional<List<Producto>> findByNombreContainingOrderByNombreAsc( String nombre); 
+
+    List<Producto> findByEstado(boolean estado);
+
+    List<Producto> findByPrecioVentaBetween (int min, int max);
 
 }
